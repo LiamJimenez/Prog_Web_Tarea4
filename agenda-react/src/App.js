e0cfc85091addcbd4b0cb8f74d49e937bcef2f2f
LiamJimenez <liammarcos@gmail.com>
2023-07-17 20:58:46 -0400

    Actualizar

--- agenda-react/src/App.js
+++ agenda-react/src/App.js
@@ -1,26 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Component from './Component';
+import ComponentJava from './ComponentJava';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
-      </header>
-    </div>
+        <Component />
+  );
+}
+
+function Java() {
+  return (
+        <ComponentJava />
   );
 }
 
---
+++ agenda-react/src/Component.jsx
@@ -0,0 +1,38 @@
+import React, { memo } from 'react'
+import './App.css';
+
+const Component = memo(() => {
+  return (
+    <div>
+        <h2>Agregar usuarios</h2>
+
+      <label for="nombre">Nombre:</label>
+      <input type="text" id="nombre" required></input>
+      <label for="apellido">Apellido:</label>
+      <input type="text" id="apellido" required></input>
+      <label for="telefono">Teléfono:</label>
+      <input type="text" id="telefono" required></input>
+
+      <div class="buttons-container">
+        <button type="button" id="agregarContacto">Agregar</button>
+        <button type="button" id="obtenerContactos">Obtener</button>
+        <button type="button" id="eliminarContactos">Eliminar</button>
+      </div>
+
+    <table id="usersTable">
+    <thead>
+      <tr>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Teléfono</th>
+      </tr>
+    </thead>
+    <tbody id="usersTableBody"></tbody>
+  </table>
+
+    </div>
+    
+  )
+})
+
+export default Component
